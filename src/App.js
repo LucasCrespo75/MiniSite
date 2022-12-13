c5f545514436388f88fa8a08f6554e557ff57309
LucasCrespo75 <lucascrespo_rodrigues@live.com>
2022-12-13 17:29:44 -0300

    primeiro commit, com os estilos e estrutura basica

---
+++ src/App.js
@@ -0,0 +1,39 @@
+//import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+//import {
+  //SafeAreaView,
+  //View,
+  //StyleSheet,
+  //Text,
+  //Linking
+//} from 'react-native';
+
+function App() {
+  const paginaMinha = "paginaMinha";
+  return (
+    <div className= 'pagina'>
+    <h1 id = {paginaMinha}>LOGO DA LOJA</h1>
+    
+      <p>AQUI É A LOGO SENDO O SITE INDICADO sgsfgfdfdgdfgfdgdfgdgdf</p>
+
+      <ul>
+        <li>
+      <button>INSTAGRAM-LINK</button>
+      </li>
+
+      <li>
+      <button>whatsapp-grupo</button>
+      </li>
+
+      <li>
+      <button>Direcionamento para atendimento</button>
+      </li>
+
+      </ul> 
+  </div>
+      
+  );
+}
+
+export default App;
